c23dc1241e3f34b2922236201d0ce2f6babfa094
Alexander A. Ramírez M <alexander.ramirez@synergy-gb.com>
2015-11-03 16:21:39 -0430

    First commit - Seed

---
+++ gulp/server.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var browserSyncSpa = require('browser-sync-spa');
+
+var util = require('util');
+
+var middleware = require('./proxy');
+
+module.exports = function(options) {
+
+  function browserSyncInit(baseDir, browser) {
+    browser = browser === undefined ? 'default' : browser;
+
+    var routes = {
+      '/bower_components': 'bower_components'
+    };
+
+    var server = {
+      baseDir: baseDir,
+      routes: routes
+    };
+
+    if(middleware.length > 0) {
+      server.middleware = middleware;
+    }
+
+    browserSync.instance = browserSync.init({
+      startPath: '/',
+      server: server,
+      browser: browser
+    });
+  }
+
+  browserSync.use(browserSyncSpa({
+    selector: '[ng-app]'// Only needed for angular apps
+  }));
+
+  gulp.task('serve', ['default', 'watch'], function () {
+    browserSyncInit('dist');
+  });
+
+  gulp.task('serve:dist', ['build'], function () {
+    browserSyncInit(options.dist);
+  });
+};
